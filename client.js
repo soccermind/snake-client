891257f7b7958069000f3a119bf2ba8379d71f82
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-04 22:30:30 +0000

    Updated client.js, input.js, play.js

--- client.js
+++ client.js
@@ -17,8 +17,6 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: AOS');
-    // setInterval( () => conn.write('Move: up'), 50);
-    // setInterval( () => conn.write('Move: left'), 100);
   });
   
   conn.on('data', (data) => {
--- input.js
+++ input.js
@@ -1,7 +1,22 @@
+// Stores the active TCP connection object.
+let connection;
+
 // \u0003 maps to ctrl+c input
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up')
+  }
+  if (key === 's') {
+    connection.write('Move: down')
+  }
+  if (key === 'a') {
+    connection.write('Move: left')
+  }
+  if (key === 'd') {
+    connection.write('Move: right')
   }
 };
 
@@ -9,8 +24,9 @@ const handleUserInput = function(key) {
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
-  const stdin = process.stdin;
+const setupInput = function(conn) {
+  connection = conn;
+  stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
